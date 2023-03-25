63a0a1743a2213bff3fa647d4b4a7bf66dfda854
Chun Lee <jim19980815@gmail.com>
2023-03-25 16:27:20 -0400

    persist reducer config

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -6,3 +6,42 @@ const initialState = {
   token: null,
   posts: [],
 };
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setmode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatePosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatePosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogOut, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
